c51488a69f519cccb1443558ecefb6f59198e254
MarDatsko <datskomar@gmail.com>
2019-12-10 14:05:26 +0200

    Added class ProgramRuner

--- src/com/homework4/task1/Main.java
+++ src/com/homework4/task1/Main.java
@@ -4,6 +4,6 @@ import com.homework4.task1.model.*;
 
 public class Main {
     public static void main(String[] args) {
-        new RunProgram().runProgram();
+        new ProgramRuner().runProgram();
     }
 }
