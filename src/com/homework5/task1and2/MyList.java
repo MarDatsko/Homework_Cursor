df026fa2f8d54450524f93c5fab16b4b7a7f5fd5
MarDatsko <datskomar@gmail.com>
2019-12-12 23:01:38 +0200

    Added opportunity check if list is null, create my class more abstraction

--- src/com/homework5/task1and2/MyList.java
+++ src/com/homework5/task1and2/MyList.java
@@ -1,33 +1,44 @@
 package com.homework5.task1and2;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MyList<T extends Comparable<T>> {
-    private ArrayList<T> array = new ArrayList<>();
+    private List<T> array = new ArrayList<>();
 
     public void add(T a) {
         array.add(a);
     }
 
-    public ArrayList<T> getArray (){
+    public List<T> getList() {
         return this.array;
     }
 
-    public <F extends Number & Comparable<F>> F getLargestNumber(ArrayList<F> array) {
-        F largest = array.get(0);
-        for (int i = 0; i < array.size(); i++) {
-            if (array.get(i).compareTo(largest) > 0) {
-                largest = array.get(i);
+    public <F extends Number & Comparable<F>> F getLargestNumber(List<F> array) {
+        F largest = null;
+        if (array.isEmpty()) {
+            System.out.println("Your array is empty");
+        } else {
+            largest = array.get(0);
+            for (int i = 0; i < array.size(); i++) {
+                if (array.get(i).compareTo(largest) > 0) {
+                    largest = array.get(i);
+                }
             }
         }
         return largest;
     }
 
-    public <F extends Number & Comparable<F>> F getSmallestNumber(ArrayList<F> array) {
-        F smallest = array.get(0);
-        for (int i = 0; i < array.size(); i++) {
-            if (array.get(i).compareTo(smallest) < 0) {
-                smallest = array.get(i);
+    public <F extends Number & Comparable<F>> F getSmallestNumber(List<F> array) {
+        F smallest = null;
+        if (array.isEmpty()) {
+            System.out.println("Your array is empty");
+        } else {
+            smallest = array.get(0);
+            for (int i = 0; i < array.size(); i++) {
+                if (array.get(i).compareTo(smallest) < 0) {
+                    smallest = array.get(i);
+                }
             }
         }
         return smallest;
